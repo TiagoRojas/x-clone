aecd64fb0c6d59853186e7c7428ad21ff9f7bc08
Tiago Rojas <rojastiago04@gmail.com>
2024-02-13 00:37:35 -0300

    Redirect fix

--- src/app/[locale]/auth/callback/route.js
+++ src/app/[locale]/auth/callback/route.js
@@ -1,16 +1,15 @@
 import {createRouteHandlerClient} from '@supabase/auth-helpers-nextjs';
 import {cookies} from 'next/headers';
-import {redirect, useRouter} from 'next/navigation';
+import {NextResponse} from 'next/server';
 
 export async function GET(req) {
 	const requestUrl = new URL(req.url);
 	const code = requestUrl.searchParams.get('code');
-	const router = useRouter();
+
 	if (code !== null) {
 		const supabase = createRouteHandlerClient({cookies});
 		await supabase.auth.exchangeCodeForSession(code);
-		router.push(requestUrl.origin);
 	}
 
-	// return redirect(requestUrl.origin);
+	return NextResponse.redirect(requestUrl.origin);
 }
